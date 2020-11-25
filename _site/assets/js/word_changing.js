264f46a3d4da6c22f71e4254f527a80ae21c9aec
llord1 <36425499+llord1@users.noreply.github.com>
2020-11-25 18:17:59 -0500

    updating path

--- _site/assets/js/word_changing.js
+++
@@ -1,9 +0,0 @@
-function swap_words(side) {
-
-	var selected_word = document.getElementById(side + "_word_chooser").value;
-	var conjugations = words[selected_word];
-
-	for (const conjugation in conjugations) {
-		document.querySelectorAll("#" + side + "_hand ." + conjugation)[0].innerHTML = conjugations[conjugation];
-	}
-}
